427a2550b20a3bc23481d87a75cdae23c9568e67
baico <yilovesky520@gmail.com>
2026-01-22 18:37:13 +0800

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,13 +1,28 @@
 { pkgs, ... }: {
   channel = "stable-24.05";
   packages = [ pkgs.unzip pkgs.wget pkgs.python3 ];
+  
+  # 所有的核心参数全在这里，实现“一个文件管全家”
+  env = {
+    USER = "root";
+    # 哪吒配置参数
+    NZ_SERVER = "nz.117.de5.net:443";
+    NZ_SECRET = "p3joFK1jc3Z31YXqMXfNPvjjxx1lQknL";
+    NZ_UUID = "ada9d899-a9d8-43ee-593b-308ac850fd3e";
+    # Argo 节点参数
+    agk = "eyJhIjoiNDc4NmQyMjRkZTJkNmM2YTcwOWRkNTIwYjZhMzczOTMiLCJ0IjoiOWJlZmZiM2YtMTc2Mi00MGU0LWJhNDgtYjEyNTU4NjM0MjQxIiwicyI6Ik5qWXhNV1ZqTW1ZdE0yVTFOQzAwTTJNMExXSmhNbVF0TkRNeE5XTTRNMkZsT1dVdyJ9";
+    agn = "idx.113.de5.net";
+    argo = "vwpt";
+    vwpt = "8001";
+  };
+
   idx = {
     workspace = {
       onCreate = {
         setup = "chmod +x *.sh";
       };
       onStart = {
-        # 核心：由这个脚本统一按顺序启动所有功能
+        # 启动唯一的指挥官脚本
         run-all = "bash start.sh";
       };
     };
@@ -15,8 +30,6 @@
       enable = true;
       previews = {
         web = {
-          # 注意这里：我们不再让 IDX 自动运行 python 命令
-          # 我们只需要在这里注册 8001 端口，启动交给 start.sh
           command = ["sleep" "infinity"]; 
           manager = "web";
         };
