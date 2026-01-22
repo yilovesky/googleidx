4564b9bf11ac1530fd553a66ae43b77dd8bce076
baico <yilovesky520@gmail.com>
2026-01-23 03:21:47 +0800

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,15 +3,12 @@
   packages = [ pkgs.unzip pkgs.wget pkgs.python3 ];
   idx.workspace = {
     onCreate.setup = "chmod +x *.sh 2>/dev/null || true";
-    onStart.run-all = "bash start.sh"; # 这里会启动 start.sh，进而执行 fun.conf 的逻辑 
+    onStart.run-all = "bash start.sh";
   };
   idx.previews.enable = true;
   idx.previews.previews.web = {
-    # 预览现在指向你新设定的 8003 端口
     command = ["sleep" "infinity"];
     manager = "web";
-    env = {
-      PORT = "8003"; # 明确指定预览监听 8003
-    };
+    env = { PORT = "8003"; }; 
   };
 }
