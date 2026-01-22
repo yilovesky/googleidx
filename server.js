5b3f9f03e6a0067d76e6b74894cdb68a6c75b837
baico <yilovesky520@gmail.com>
2026-01-23 01:28:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const http = require('http'), fs = require('fs');
-const port = process.env.PORT || 8080;
+const port = 8001; // 对接 env.conf 中的 vwpt
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    try { 
-        // 这里读取的是同目录下的 index.html
-        res.end(fs.readFileSync('index.html')); 
-    } catch (e) { 
-        res.end('<h1>Error: index.html Not Found</h1>'); 
+    try {
+        res.end(fs.readFileSync('index.html'));
+    } catch (e) {
+        res.end('<h1>Monitor Active</h1><p>iOS 26 UI file not found.</p>');
     }
-}).listen(port, '0.0.0.0', () => console.log('Server is running on ' + port));
+}).listen(port, '0.0.0.0');
