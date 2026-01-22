ffb2b406da4ac7ba12b73a7b1efce0b365536bba
baico <yilovesky520@gmail.com>
2026-01-23 01:57:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http'), fs = require('fs');
-const port = 8080; // 修改这里，避开隧道的 8001
+const port = 8080; // 这里的端口必须改为 8080
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     try {
         res.end(fs.readFileSync('index.html'));
     } catch (e) {
-        res.end('<h1>Monitor Active</h1>');
+        res.end('<h1>Monitor System Active</h1>');
     }
-}).listen(port, '0.0.0.0', () => console.log('UI Web running on 8080'));
+}).listen(port, '0.0.0.0', () => console.log('iOS 26 UI Running on 8080'));
