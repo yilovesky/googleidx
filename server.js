c511dc599e7759083735d18f5c3b0212ce2bf60d
baico <yilovesky520@gmail.com>
2026-01-23 01:47:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http'), fs = require('fs');
-const port = 8001; // 对接 env.conf 中的 vwpt
+const port = 8080; // 修改这里，避开隧道的 8001
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     try {
         res.end(fs.readFileSync('index.html'));
     } catch (e) {
-        res.end('<h1>Monitor Active</h1><p>iOS 26 UI file not found.</p>');
+        res.end('<h1>Monitor Active</h1>');
     }
-}).listen(port, '0.0.0.0');
+}).listen(port, '0.0.0.0', () => console.log('UI Web running on 8080'));
